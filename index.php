8b9e2e917414af914bef5c7c8894e1a1e1c3bf0b
Jhonathan Sinzervch <jhonathan.con@outlook.com>
2018-07-24 17:09:38 -0300

    atualizacao ok

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Ola Mundo abc!";
 
 	echo "Ola udemy 123!"
-=======
-	echo "Ola Mundo 789!";
 
-	echo "Ola udemy abc!"
->>>>>>> 8e9f7d56e1c0991130add3f2a9b5bff5ad60dcd3
+
 
 ?>
